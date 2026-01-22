dc34286b43551ed74fba9dc273cf3f4e2a8af8bc
enfia <enfiaxd@gmail.com>
2026-01-22 16:37:54 +0900

    202601221637

---
+++ ch01_js_basic/02_variable.js
@@ -0,0 +1,13 @@
+var x = 5;
+var y = 6;
+var z = x + y;
+
+console.log(z);
+
+x = '안녕123';
+z = x+y;
+console.log(z);
+console.log(a); // undefined 라고 출력됨
+var a = '안녕하세요';
+console.log(a);
+console.log('');
---
+++ ch01_js_basic/03_let_variable.js
@@ -0,0 +1,7 @@
+let x = 5;
+let y = 6;
+let z = x + y;
+console.log(z);
+x = 100;
+z = x + y;
+console.log(z);
